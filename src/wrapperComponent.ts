46958fc013be4bed6ca52afa2033d6b8c780e942
geekris1 <200949970@qq.com>
2022-09-12 21:00:22 +0800

    chore:remove /

--- src/wrapperComponent.ts
+++ src/wrapperComponent.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 import { cwd } from 'process'
 const c = cwd()
-\export function getComponentPath(markdownPath: string, componentPath: string): string {
+export function getComponentPath(markdownPath: string, componentPath: string): string {
   if (!path.isAbsolute(componentPath))
     componentPath = path.resolve(c, componentPath)
   const t = path.relative(path.dirname(markdownPath), componentPath)
